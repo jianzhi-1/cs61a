15805e1337ce53e7803333287919ec37c850e2eb
Jianzhi Wang <jianzhiw00@gmail.com>
2021-08-26 22:12:53 -0700

    Create higher-ordered-fn.py

---
+++ higher-ordered-fn.py
@@ -0,0 +1,18 @@
+### Function Decorators
+### @ is the decorator and the function is evaluated first
+### then, def is executed
+### then, result of evaluating decorated expression is applied on newly defined fn
+### then, result is bound to the name in def
+
+def trace(f):
+    def wrapped(x):
+        print('-> {}({})'.format(f, x))
+        return f(x)
+    return wrapped
+
+@trace
+def fib(x):
+    if (x == 0 or x == 1): return 1
+    return fib(x - 1) + fib(x - 2)
+
+print(fib(3))
