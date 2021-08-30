dc2fe66f919e5f490160f17d496cda4843ff8f3b
Jianzhi Wang <jianzhiw00@gmail.com>
2021-08-30 14:57:53 -0700

    Create lec3_prime.py

---
+++ lec3_prime.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+  return (n > 1) and smallest_factor(n) == n
+
+def smallest_factor(n):
+  k = 2
+  while (k <= n):
+    if (n % k == 0): return k
+    k += 1
+
+def print_factors(n):
+  while (n != 1):
+    x = smallest_factor(n)
+    print(x)
+    n /= x
